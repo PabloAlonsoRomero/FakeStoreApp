0a7560f4065843fee7a425f7dd8ab300dfcc13ab
PabloEmilioAlonsoRomero <157993849+PabloAlonsoRomero@users.noreply.github.com>
2025-04-04 00:07:49 -0600

    Escritura en searchBar

--- app/src/main/java/com/example/fakestoreapp/components/SearchBar.kt
+++ app/src/main/java/com/example/fakestoreapp/components/SearchBar.kt
@@ -1,5 +1,6 @@
 package com.example.fakestoreapp.components
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,10 +10,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,12 +28,20 @@ import com.example.fakestoreapp.ui.theme.FakeStoreAppTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchBar() {
+    var textSearch by remember {
+        mutableStateOf("")
+    }
+
+
     Column (
         modifier = Modifier.padding(horizontal = 16.dp)
     ) {
         TextField(
-            value = "",
-            onValueChange = {},
+            value = textSearch,
+            onValueChange = { valor ->
+                textSearch = valor
+                Log.i("TextField", valor)
+            },
             placeholder = { Text("Busca aqui") },
             trailingIcon = { Icon(Icons.Default.Search, contentDescription = "Icono de busqueda") },
             modifier = Modifier.fillMaxWidth(),
